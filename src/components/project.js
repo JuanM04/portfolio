d6ce9f832c1e4e71de91ec1c9483649c1b7d9fb7
JuanM04 <me@juanm04.com>
2019-04-19 16:22:00 -0300

    webp

--- src/components/project.js
+++ src/components/project.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
-export default props => (
-  <a
-    href={props.url}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="Project"
-  >
-    <img src={require(`../images/projects/${props.image}.png`)} alt={props.name}/>
+export default props => {
+  const imagePNG = require(`../images/projects/${props.image}.png`)
+  const imageWEBP = require(`../images/projects/${props.image}.webp`)
 
-    <h2>{props.name}</h2>
-    <h3>{props.category}</h3>
-    <p>{props.description}</p>
-  </a>
-)
+  return(
+    <a
+      href={props.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="Project"
+    >
+      <picture>
+        <source type="image/webp" srcset={imageWEBP} />
+        <source type="image/png" srcset={imagePNG} />
+        <img src={imagePNG} alt={props.name}/>
+      </picture>
+
+      <h2>{props.name}</h2>
+      <h3>{props.category}</h3>
+      <p>{props.description}</p>
+    </a>
+  )
+}
