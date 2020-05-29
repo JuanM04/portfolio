4242edb410550cda920ef068f5520f9fe5da1d7f
Juan Martín Seery <me@juanm04.com>
2020-05-29 11:18:39 -0300

    Changed unused parameter to `_` to make TS happy

--- pages/api/get-to.ts
+++ pages/api/get-to.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { getGist } from "utils/helpers"
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (_: NextApiRequest, res: NextApiResponse) => {
   const files = await getGist(process.env.GIST_TO as string)
   return res.send(JSON.parse(files[0].text))
 }
