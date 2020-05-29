5c021c5177e0728eaf52bdb6fc8ca1bd8203efb7
Juan Martín Seery <me@juanm04.com>
2020-05-29 11:03:58 -0300

    [To] loading cursor

--- pages/to/index.tsx
+++ pages/to/index.tsx
@@ -6,7 +6,6 @@ import slugify from "slugify"
 
 import { Layout } from "components"
 import styles from "styles/to"
-import { copyFile } from "fs"
 
 export default () => {
   const { data: redirects, error } = useSWR<Redirects>("/api/get-to", (url) =>
@@ -46,84 +45,88 @@ export default () => {
 
   return (
     <Layout title="To">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          if (loading || !redirects) return
-          update({
-            ...redirects,
-            [slugify(inputs.name, { lower: true })]: {
-              name: inputs.name,
-              url: inputs.url,
-            },
-          })
-        }}
-      >
-        <input
-          type="text"
-          placeholder="Name"
-          value={inputs.name}
-          onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
-          disabled={loading || !redirects}
-          required
-        />
-        <br />
-        <input
-          type="url"
-          placeholder="URL"
-          value={inputs.url}
-          onChange={(e) => setInputs({ ...inputs, url: e.target.value })}
-          disabled={loading || !redirects}
-          required
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Admin Password"
-          value={inputs.password}
-          onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-          disabled={loading || !redirects}
-          required
-        />
-        <br />
-        <button type="submit">Create</button>
-      </form>
-      <hr />
-      {!redirects ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {Object.keys(redirects)
-            .sort((a, b) => redirects[a].name.localeCompare(redirects[b].name))
-            .map((slug) => (
-              <li key={slug} className={styles.url}>
-                <a
-                  href={redirects[slug].url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {redirects[slug].name}
-                </a>
-                <FontAwesomeIcon
-                  icon={faCopy}
-                  onClick={() =>
-                    navigator.clipboard.writeText(`${location.href}/${slug}`)
-                  }
-                />
-                {!loading && inputs.password.length > 1 && (
+      <div className={loading || !redirects ? styles.loading : ""}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            if (loading || !redirects) return
+            update({
+              ...redirects,
+              [slugify(inputs.name, { lower: true })]: {
+                name: inputs.name,
+                url: inputs.url,
+              },
+            })
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
+            disabled={loading || !redirects}
+            required
+          />
+          <br />
+          <input
+            type="url"
+            placeholder="URL"
+            value={inputs.url}
+            onChange={(e) => setInputs({ ...inputs, url: e.target.value })}
+            disabled={loading || !redirects}
+            required
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Admin Password"
+            value={inputs.password}
+            onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+            disabled={loading || !redirects}
+            required
+          />
+          <br />
+          <button type="submit">Create</button>
+        </form>
+        <hr />
+        {!redirects ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {Object.keys(redirects)
+              .sort((a, b) =>
+                redirects[a].name.localeCompare(redirects[b].name)
+              )
+              .map((slug) => (
+                <li key={slug} className={styles.url}>
+                  <a
+                    href={redirects[slug].url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {redirects[slug].name}
+                  </a>
                   <FontAwesomeIcon
-                    icon={faTrash}
-                    onClick={() => {
-                      let newRedirects = Object.assign({}, redirects)
-                      delete newRedirects[slug]
-                      update(newRedirects)
-                    }}
+                    icon={faCopy}
+                    onClick={() =>
+                      navigator.clipboard.writeText(`${location.href}/${slug}`)
+                    }
                   />
-                )}
-              </li>
-            ))}
-        </ul>
-      )}
+                  {!loading && inputs.password.length > 1 && (
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      onClick={() => {
+                        let newRedirects = Object.assign({}, redirects)
+                        delete newRedirects[slug]
+                        update(newRedirects)
+                      }}
+                    />
+                  )}
+                </li>
+              ))}
+          </ul>
+        )}
+      </div>
     </Layout>
   )
 }
