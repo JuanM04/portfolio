4a9fc84e8189ac04b9c82ed119fc2e1bfc31b8ff
JuanM04 <me@juanm04.com>
2022-03-26 15:47:14 -0300

    chore: ⬆️ upgraded dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,10 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
-
-import type { AstroUserConfig } from "astro"
+import { defineConfig } from "astro/config"
 import markdownRemark, { AstroMarkdownOptions } from "@astrojs/markdown-remark"
+import solid from "@astrojs/solid-js"
+import sitemap from "@astrojs/sitemap"
+
 import { betterImages } from "./src/plugins/better-images"
 import { getLanguages } from "./src/plugins/languages"
 
@@ -28,17 +30,18 @@ const markdownConfig: AstroMarkdownOptions = {
   ],
 }
 
-const config: AstroUserConfig = {
-  renderers: ["@astrojs/renderer-solid"],
+// https://astro.build/config
+export default defineConfig({
+  integrations: [solid(), sitemap()],
   buildOptions: {
     site: "https://juanm04.com",
-    experimentalStaticBuild: true,
-    sitemap: true,
   },
   markdownOptions: {
     render: [markdownRemark, markdownConfig],
   },
-  vite: { ssr: { external: ["svgo"] } },
-}
-
-export default config
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+})
