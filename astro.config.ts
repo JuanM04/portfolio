b2cabce15467c6c4720713321cb8dea52b5ad6a2
JuanM04 <me@juanm04.com>
2022-04-21 16:59:38 -0300

    chore: ⬆️ Upgraded to Astro beta

--- astro.config.ts
+++ astro.config.ts
@@ -1,43 +1,36 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 import { defineConfig } from "astro/config"
-import markdownRemark, { AstroMarkdownOptions } from "@astrojs/markdown-remark"
 import solid from "@astrojs/solid-js"
 import sitemap from "@astrojs/sitemap"
 
 import { betterImages } from "./src/plugins/better-images"
 import { getLanguages } from "./src/plugins/languages"
 
-const markdownConfig: AstroMarkdownOptions = {
-  syntaxHighlight: "shiki",
-  shikiConfig: {
-    theme: "css-variables",
-    langs: getLanguages(),
-  },
-  remarkPlugins: ["remark-gfm", "remark-smartypants", "remark-code-titles", "remark-math"],
-  rehypePlugins: [
-    "rehype-slug",
-    [
-      "rehype-katex",
-      {
-        output: "html",
-        macros: {
-          "\\unit": "\\ \\text{#1}",
-        },
-      },
-    ],
-    betterImages,
-  ],
-}
-
 // https://astro.build/config
 export default defineConfig({
+  site: "https://juanm04.com",
   integrations: [solid(), sitemap()],
-  buildOptions: {
-    site: "https://juanm04.com",
-  },
-  markdownOptions: {
-    render: [markdownRemark, markdownConfig],
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "css-variables",
+      langs: getLanguages(),
+    },
+    remarkPlugins: ["remark-gfm", "remark-smartypants", "remark-code-titles", "remark-math"],
+    rehypePlugins: [
+      "rehype-slug",
+      [
+        "rehype-katex",
+        {
+          output: "html",
+          macros: {
+            "\\unit": "\\ \\text{#1}",
+          },
+        },
+      ],
+      betterImages,
+    ],
   },
   vite: {
     ssr: {
--- src/plugins/better-images.ts
+++ src/plugins/better-images.ts
@@ -1,11 +1,10 @@
-import type { Plugin } from "unified"
-import type { Root } from "hast"
+import type { RehypePlugin } from "@astrojs/markdown-remark"
 import { visit } from "unist-util-visit"
 
 const videoTest = /\/(.*)(.mp4|.mov)$/
 
-export const betterImages: Plugin = () => tree => {
-  visit(tree as Root, "element", (node, _index, parent) => {
+export const betterImages: RehypePlugin<[]> = () => tree => {
+  visit(tree, "element", (node, _index, parent) => {
     if (node.tagName !== "img") return
     if (parent && parent.type === "element" && parent.tagName === "figure") return
 
