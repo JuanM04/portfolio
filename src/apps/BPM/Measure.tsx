81e351c21d2b9a45d7a4e6d4166a9254427cc7b2
JuanM04 <me@juanm04.com>
2022-07-16 15:37:02 -0300

    feat(bpm): ✨ Improved BPM

--- src/apps/BPM/Measure.tsx
+++ src/apps/BPM/Measure.tsx
@@ -3,8 +3,24 @@ import { createMemo, createSignal } from "solid-js"
 
 type Timestamps = [current: number, previous: number] | null
 
+function median(inputs: number[]) {
+  if (inputs.length === 0) return 0
+
+  let values = [...inputs]
+  values.sort((a, b) => a - b)
+
+  var half = Math.floor(values.length / 2)
+
+  if (values.length % 2) return values[half]
+
+  return (values[half - 1] + values[half]) / 2.0
+}
+
 export function Measure() {
   const [timestamps, setTimestamps] = createSignal<Timestamps>(null, {
+    equals: false,
+  })
+  const [_, setPreviousBPM] = createSignal<number[]>([], {
     equals: false,
   })
 
@@ -13,14 +29,22 @@ export function Measure() {
     if (!time) return 0
 
     const diffInSeconds = (time[0] - time[1]) / 1000
-    if (diffInSeconds <= 0 || diffInSeconds > 60) return 0
+    if (diffInSeconds <= 0 || diffInSeconds > 60) {
+      setPreviousBPM([])
+      return 0
+    }
 
-    return Math.round(60 / diffInSeconds)
+    const newBPM = 60 / diffInSeconds
+    const allBPMS = setPreviousBPM(previousBPM =>
+      previousBPM.length < 10 ? [...previousBPM, newBPM] : [...previousBPM.slice(1), newBPM]
+    )
+
+    return Math.round(median(allBPMS))
   })
 
   return (
     <button
-      class="max-w-md w-full h-full bg-red-500"
+      class="max-w-md block my-4 mx-auto w-full h-full bg-red-500"
       onClick={() => {
         setTimestamps(prev => {
           const now = new Date().getTime()
--- src/apps/BPM/Play.tsx
+++ src/apps/BPM/Play.tsx
@@ -3,8 +3,11 @@ import { createSignal } from "solid-js"
 
 type Playing = ReturnType<typeof setInterval> | false
 
+const MAX_BPM = 420
+const MIN_BPM = 1
+
 export function Play() {
-  const [bpm, setBpm] = createSignal(120)
+  const [bpm, setBpm] = createSignal(60)
   const [playing, setPlaying] = createSignal<Playing>(false)
 
   const playBeat = () => {
@@ -25,72 +28,88 @@ export function Play() {
 
   return (
     <div>
-      <div>
+      <div class="text-display flex items-center justify-center gap-x-2">
+        <button
+          class="font-black text-3xl p-2"
+          onClick={() => {
+            const newBPM = bpm() - 10
+            if (newBPM < MIN_BPM) updateBpm(MIN_BPM)
+            else updateBpm(newBPM)
+          }}
+        >
+          -
+        </button>
         <input
-          type="range"
-          min={1}
-          max={420}
-          step={1}
-          value={bpm()}
-          onInput={e => updateBpm(e.currentTarget.valueAsNumber)}
-        />
-        <input
+          class="font-bold text-5xl w-20 text-center bg-transparent outline-none no-spinner focus:border-b border-current"
           type="number"
           value={bpm()}
-          onInput={e => {
-            const value = e.currentTarget.valueAsNumber
-            if (isFinite(value) && value % 1 === 0 && value > 0 && value <= 420) {
-              updateBpm(value)
+          max={420}
+          placeholder="60"
+          min={1}
+          onChange={e => {
+            const input = e.currentTarget.value.replace(/\D/, "")
+            if (input === "") {
+              updateBpm(60)
+              return
             }
+
+            const value = parseInt(input)
+            if (!isFinite(value)) return
+
+            if (value > MAX_BPM) updateBpm(MAX_BPM)
+            else if (value < MIN_BPM) updateBpm(MIN_BPM)
+            else updateBpm(value)
           }}
         />
-      </div>
-
-      <div>
         <button
-          onClick={() =>
-            setPlaying(playing => {
-              if (typeof playing === "number") {
-                clearInterval(playing)
-                return false
-              } else {
-                playBeat()
-                const interval = getBeatInterval(bpm())
-                return interval
-              }
-            })
-          }
+          class="font-black text-3xl p-2"
+          onClick={() => {
+            const newBPM = bpm() + 10
+            if (newBPM > MAX_BPM) updateBpm(MAX_BPM)
+            else updateBpm(newBPM)
+          }}
         >
-          {playing() ? (
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <rect x="6" y="4" width="4" height="16" />
-                <rect x="14" y="4" width="4" height="16" />
-              </svg>
-              Pause
-            </span>
-          ) : (
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <polygon points="5 3 19 12 5 21 5 3" />
-              </svg>
-              Play
-            </span>
-          )}
+          +
         </button>
       </div>
+
+      <button
+        class={[
+          "flex items-center justify-center gap-x-2 mx-auto py-1 px-2 mt-2",
+          playing() ? "text-white bg-red-500" : "bg-stone-200 dark:bg-stone-700",
+        ].join(" ")}
+        onClick={() =>
+          setPlaying(playing => {
+            if (typeof playing === "number") {
+              clearInterval(playing)
+              return false
+            } else {
+              playBeat()
+              const interval = getBeatInterval(bpm())
+              return interval
+            }
+          })
+        }
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="w-5 h-5 fill-current"
+        >
+          {playing() ? (
+            <>
+              <rect x="6" y="4" width="4" height="16" />
+              <rect x="14" y="4" width="4" height="16" />
+            </>
+          ) : (
+            <polygon points="5 3 19 12 5 21 5 3" />
+          )}
+        </svg>
+        <span>{playing() ? "Pause" : "Play"}</span>
+      </button>
     </div>
   )
 }
