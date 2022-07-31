8bd373916ae572e21fd1a9e08d0605b4797e0d28
JuanM04 <me@juanm04.com>
2022-07-31 18:19:53 -0300

    refactor(docs): 🚚 Moved custom prose stylings to tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,51 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   darkMode: "media",
   theme: {
-    extend: {},
+    extend: {
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": theme("colors.red.800"),
+            "--tw-prose-bullets": theme("colors.red.500"),
+            a: {
+              textDecoration: `underline ${theme("colors.red.500")}`,
+            },
+            blockquote: {
+              fontWeight: "inherit",
+              fontStyle: "inherit",
+              color: theme("colors.stone.700"),
+              quotes: "none",
+            },
+            "blockquote p:first-of-type::before, blockquote p:first-of-type::after": {
+              content: "none",
+            },
+            "h1, h2, h3, h4, h5, h6, th": {
+              // text-display
+              fontStretch: "normal",
+              color: "#000000",
+            },
+            figcaption: {
+              fontStyle: "italic",
+              textAlign: "center",
+            },
+            video: {
+              marginTop: "0",
+              marginBottom: "0",
+            },
+          },
+        },
+        invert: {
+          css: {
+            blockquote: {
+              color: theme("colors.stone.400"),
+            },
+            "h1, h2, h3, h4, h5, h6, th": {
+              color: "#ffffff",
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
