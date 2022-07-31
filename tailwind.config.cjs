723618dbd3c222d4d94b38877daab963ed806dda
JuanM04 <me@juanm04.com>
2022-07-31 19:14:16 -0300

    fix(docs): 💄 Removed some vars

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,10 +5,8 @@ module.exports = {
   theme: {
     extend: {
       typography: theme => ({
-        DEFAULT: {
+        stone: {
           css: {
-            "--tw-prose-body": theme("colors.red.800"),
-            "--tw-prose-bullets": theme("colors.red.500"),
             a: {
               textDecoration: `underline ${theme("colors.red.500")}`,
             },
@@ -21,14 +19,14 @@ module.exports = {
             "blockquote p:first-of-type::before, blockquote p:first-of-type::after": {
               content: "none",
             },
+            figcaption: {
+              fontStyle: "italic",
+              textAlign: "center",
+            },
             "h1, h2, h3, h4, h5, h6, th": {
               // text-display
               fontStretch: "normal",
               color: "#000000",
-            },
-            figcaption: {
-              fontStyle: "italic",
-              textAlign: "center",
             },
             video: {
               marginTop: "0",
